e0c104d153fb4dc86da67d0028b58074f5f0a1b4
Juan <jsebastian.delgadillo@gmail.com>
2020-09-13 22:31:55 -0300

    Firts Commit

---
+++ src/components/Location.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Location = (props) => 
+{
+    return (
+    <div><h1>{props.city}</h1></div>
+    ) 
+}
+
+export default Location;
---
+++ src/components/WeatherData.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherTemperature from './WeatherTemperature';
+import WeatherExtraInfo from './WeatherExtraInfo';
+
+const WeatherData = () => 
+(
+    <div>
+    <WeatherTemperature></WeatherTemperature>
+    <WeatherExtraInfo></WeatherExtraInfo>
+    </div>
+)
+
+export default WeatherData;
